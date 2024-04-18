8c3554e2e1f4d91e362b67e7d019e95d1e98459f
sesam-me <lsi6601@gmail.com>
2024-04-19 00:04:59 +0900

    eureka 설정

--- src/main/java/com/example/villion_eureka_service/VillionEurekaServiceApplication.java
+++ src/main/java/com/example/villion_eureka_service/VillionEurekaServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.villion_eureka_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // 이 어노테이션으로 유레카 서버의 역할로서 서버로 역할을 해서 스프링 부트에 의해 기동
 public class VillionEurekaServiceApplication {
 
     public static void main(String[] args) {
