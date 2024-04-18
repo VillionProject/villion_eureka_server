48e6f5725b7ea4cf3c3f15722e2b008666354c4e
sesam-me <lsi6601@gmail.com>
2024-04-18 23:52:42 +0900

    first commit

---
+++ src/main/java/com/example/villion_eureka_service/VillionEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.villion_eureka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillionEurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillionEurekaServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/villion_eureka_service/VillionEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.villion_eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillionEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
